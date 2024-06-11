6f47a3ab4b0fa7ccfc831a1eaf7ba521eca192c1
anupyldd <139476397+anupyldd@users.noreply.github.com>
2024-06-11 18:45:10 +0300

    add convenience methods

--- src/logging.h
+++ src/logging.h
@@ -2,7 +2,7 @@
  * Anything with a name starting with an underscore 
  * is not supposed to be used directly.
  *
- * For logging use the EntryBuilder class 
+ * For logging use the defined LOG macros class 
  */
 
 #pragma once
@@ -41,7 +41,7 @@ namespace log
 	
 	class _Entry
 	{
-		LEVEL m_level;
+		LEVEL m_level = LEVEL::INFO;
 		std::string m_text,
 					m_fileName,
 					m_line,
@@ -71,27 +71,35 @@ namespace log
 //				Main Logging Class
 /*****************************************************/
 	
-	class EntryBuilder : private _Entry
+	class _EntryBuilder : private _Entry
 	{
 	public:
-		EntryBuilder(const std::source_location loc
+		_EntryBuilder(const std::source_location loc
 					= std::source_location::current());
-		EntryBuilder& Text(const std::string& txt);
-		EntryBuilder& Level(LEVEL lvl);
-		EntryBuilder& Channel(std::unique_ptr<_Channel> chn);
-		~EntryBuilder();
+		
+		_EntryBuilder& Text(const std::string& txt);
+		_EntryBuilder& Level(LEVEL lvl);
+		_EntryBuilder& Channel(std::unique_ptr<_Channel> chn);
+		
+		_EntryBuilder& Trace(const std::string& txt);
+		_EntryBuilder& Debug(const std::string& txt);
+		_EntryBuilder& Info(const std::string& txt);
+		_EntryBuilder& Warn(const std::string& txt);
+		_EntryBuilder& Fatal(const std::string& txt);
+		
+		~_EntryBuilder();
+		
 	private:
 		std::unique_ptr<_Channel> m_dest;
 	}
 	
 /*****************************************************/
-//				EntryBuilder Implementation
+//				_EntryBuilder Implementation
 /*****************************************************/
 	
-	EntryBuilder::EntryBuilder(const std::source_location loc)
+	_EntryBuilder::_EntryBuilder(const std::source_location loc)
 		: _Entry 
 		{
-			.m_level = LEVEL::INFO,
 			.m_fileName = loc.file_name();
 			.m_line = loc.line();
 			.m_column = loc.column();
@@ -100,26 +108,53 @@ namespace log
 	{
 		
 	}
-	EntryBuilder& EntryBuilder::Text(const std::string& txt)
+	_EntryBuilder& _EntryBuilder::Text(const std::string& txt)
 	{
 		m_text = std::move(txt);
 		return *this;
 	}
-	EntryBuilder& EntryBuilder::Level(LEVEL lvl)
+	_EntryBuilder& _EntryBuilder::Level(LEVEL lvl)
 	{
 		m_level = std::move(lvl);
 		return *this;
 	}
-	EntryBuilder& EntryBuilder::Channel(std::unique_ptr<_Channel> chn)
+	_EntryBuilder& _EntryBuilder::Channel(std::unique_ptr<_Channel> chn)
 	{
 		m_dest = std::move(chn);
 		return *this;
 	}
-	EntryBuilder::~EntryBuilder()
+	// TODO
+	_EntryBuilder& Trace(const std::string& txt)
+	{
+		Level(LEVEL::TRACE).Text(txt);
+		return *this;
+	}
+	_EntryBuilder& Debug(const std::string& txt)
+	{
+		Level(LEVEL::DEBUG).Text(txt);
+		return *this;
+	}
+	_EntryBuilder& Info(const std::string& txt)
+	{
+		Level(LEVEL::INFO).Text(txt);
+		return *this;
+	}
+	_EntryBuilder& Warn(const std::string& txt)
+	{
+		Level(LEVEL::WARN).Text(txt);
+		return *this;
+	}
+	_EntryBuilder& Fatal(const std::string& txt)
+	{
+		Level(LEVEL::FATAL).Text(txt);
+		return *this;
+	}
+	
+	_EntryBuilder::~_EntryBuilder()
 	{
 		if(m_dest) m_dest->Submit();
 	}
 }
 
-#define LOG log::EntryBuilder{std::source_location::current()}
+#define LOG log::_EntryBuilder{std::source_location::current()}
 }
