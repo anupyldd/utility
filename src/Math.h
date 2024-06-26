4a557a58820a29ba16a509d84b9d7460922097cc
anupyldd <139476397+anupyldd@users.noreply.github.com>
2024-06-26 13:13:46 +0300

    more members to vec2

--- src/Math.h
+++ src/Math.h
@@ -17,6 +17,9 @@
 #include <iostream>
 #include <any>
 #include <type_traits>
+#include <cmath>
+#include <string>
+#include <format>
 
 namespace util
 {
@@ -37,6 +40,22 @@ namespace math
 		Vec2(T x, T y) : x(x), y(y) {}
 		Vec2(const Vec2<T>& src) : x(src.x), y(src.y) {}
 
+		void Zero() { x = y = 0; }
+		
+		T Sum() { return x + y; }
+		T Sub() { return x - y; }
+		T Mul() { return x * y; }
+		T Div() { return x / y; }
+		T Avg() { return (x + y) / 2; }
+
+		T Min() { return std::min(x, y); }
+		T Max() { return std::max(x, y); }
+		float LenSq() { return x * x + y * y; }
+		float Len() { return std::sqrt(LenSq()); }
+
+		std::string ToStr() { return std::format("{}, {}", x, y); }
+
+	public:
 		template<class NT>
 		operator Vec2<NT>() const { return Vec2<NT>{(NT)x, (NT)y}; }
 
@@ -55,10 +74,6 @@ namespace math
 			os << v.x << ", " << v.y;
 			return os;
 		}
-
-		void ToZero() { x = y = 0; }
-		T Sum() { return x + y; }
-
 	};
 
 }
