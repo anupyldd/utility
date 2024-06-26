e5fd2e3707cb1bb622b3c12d394ec20c5e6f745d
anupyldd <139476397+anupyldd@users.noreply.github.com>
2024-06-26 17:00:43 +0300

    add vec4

--- src/Math.h
+++ src/Math.h
@@ -9,7 +9,7 @@
  * - Vec2 
  * - Vec3
  * 
- * There are also some aliases like position, point,
+ * There are also some aliases like position, point, color,
  * etc. already defined in the format <name><dimensions><data type>
  */
 
@@ -33,13 +33,13 @@ namespace math
 //				Constants
 /*****************************************************/
 
-	using PI = std::numbers::pi;
-	using PI2 = PI * 2;
+	constexpr double PI = 3.1415926535897932;
+	constexpr double PI2 = PI * 2;
 
-	using PI_2 = PI / 2;
-	using PI_3 = PI / 3;
-	using PI_4 = PI / 4;
-	using PI_6 = PI / 6;
+	constexpr double PI_2 = PI / 2;
+	constexpr double PI_3 = PI / 3;
+	constexpr double PI_4 = PI / 4;
+	constexpr double PI_6 = PI / 6;
 
 /*****************************************************/
 //				Structures
@@ -149,7 +149,7 @@ namespace math
 
 		Vec3& operator+=(T v) { x += v; y += v; z += v; return *this; }
 		Vec3& operator-=(T v) { x -= v; y -= v; z -= v; return *this; }
-		Vec3& operator*=(T v) { x *= v; y *= v; z += v; return *this; }
+		Vec3& operator*=(T v) { x *= v; y *= v; z *= v; return *this; }
 		Vec3& operator/=(T v) { x /= v; y /= v; z /= v; return *this; }
 
 		Vec3& operator+=(const Vec3& v) { x += v.x; y += v.y; z += v.z; return *this; }
@@ -183,6 +183,74 @@ namespace math
 		}
 	};
 
+	template<class T>
+	struct Vec4
+	{
+		T x, y, z, w;
+
+	public:
+		Vec4() = default;
+		Vec4(T v) : x(x), y(x), z(x), w(x) {}
+		Vec4(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
+		Vec4(const Vec4<T>& src) : x(src.x), y(src.y), z(src.z), w(src.w) {}
+
+		T Sum() const { return x + y + z + w; }
+		T Mul() const { return x * y * z * w; }
+		T Avg() const { return (x + y + z + w) / 4; }
+
+		T Min() const { return std::min({ x,y,z,w }); }
+		T Max() const { return std::max({ x,y,z,w }); }
+		double MagSq() const { return x * x + y * y + z * z + w * w; }
+		double Mag() const { return std::sqrt(MagSq()); }
+
+		double Dot(const Vec4& v) { return x * v.x + y * v.y + z * v.z + w * v.w; }
+
+		void Zero() { x = y = z = w = 0; }
+
+		Vec4<double> Normalize() const { double mag = Mag(); return (mag == 0) ? Vec4<double>(x, y, z, w) : *this / mag; }
+
+		std::string ToStr() const { return std::format("{}, {}, {}, {}", x, y, z, w); }
+
+	public:
+		template<class NT>
+		operator Vec4<NT>() const { return Vec4<NT>{(NT)x, (NT)y}; }
+
+		Vec4& operator+=(T v) { x += v; y += v; z += v; w += v; return *this; }
+		Vec4& operator-=(T v) { x -= v; y -= v; z -= v; w -= v; return *this; }
+		Vec4& operator*=(T v) { x *= v; y *= v; z *= v; w *= v; return *this; }
+		Vec4& operator/=(T v) { x /= v; y /= v; z /= v; w /= v; return *this; }
+
+		Vec4& operator+=(const Vec4& v) { x += v.x; y += v.y; z += v.z; w += v.w; return *this; }
+		Vec4& operator-=(const Vec4& v) { x -= v.x; y -= v.y; z -= v.z; w -= v.w; return *this; }
+		Vec4& operator*=(const Vec4& v) { x *= v.x; y *= v.y; z *= v.z; w *= v.w; return *this; }
+		Vec4& operator/=(const Vec4& v) { x /= v.x; y /= v.y; z /= v.z; w /= v.w; return *this; }
+
+		bool operator==(const Vec4& v) { return x == v.x && y == v.y && z == v.z && w == v.w; }
+		bool operator!=(const Vec4& v) { return x != v.x || y != v.y || z != v.z || w != v.w; }
+		bool operator>(const Vec4& v) { return MagSq() > v.MagSq(); }
+		bool operator>=(const Vec4& v) { return MagSq() >= v.MagSq(); }
+		bool operator<(const Vec4& v) { return MagSq() < v.MagSq(); }
+		bool operator<=(const Vec4& v) { return MagSq() <= v.MagSq(); }
+
+		template<class C>
+		friend Vec4<C> operator+(const Vec4<T>& lhs, const C& rhs) { return Vec4<C>(lhs.x + rhs, lhs.y + rhs, lhs.z + rhs, lhs.w + rhs); }
+
+		template<class C>
+		friend Vec4<C> operator-(const Vec4<T>& lhs, const C& rhs) { return Vec4<C>(lhs.x - rhs, lhs.y - rhs, lhs.z - rhs, lhs.w - rhs); }
+
+		template<class C>
+		friend Vec4<C> operator*(const Vec4<T>& lhs, const C& rhs) { return Vec4<C>(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs, lhs.w * rhs); }
+
+		template<class C>
+		friend Vec4<C> operator/(const Vec4<T>& lhs, const C& rhs) { return Vec4<C>(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs, lhs.w / rhs); }
+
+		friend std::ostream& operator<<(std::ostream& os, const Vec4<T>& v)
+		{
+			os << v.x << ", " << v.y << ", " << v.z << ", " << v.w;
+			return os;
+		}
+	};
+
 /*****************************************************/
 //				Aliases
 /*****************************************************/
@@ -211,6 +279,26 @@ namespace math
 	using Pos3I = Vec3<int>;
 	using Pos3F = Vec3<float>;
 	using Pos3D = Vec3<double>;
+
+
+	using Vec4I = Vec4<int>;
+	using Vec4F = Vec4<float>;
+	using Vec4D = Vec4<double>;
+
+	using Point4I = Vec4<int>;
+	using Point4F = Vec4<float>;
+	using Point4D = Vec4<double>;
+
+	using Pos4I = Vec4<int>;
+	using Pos4F = Vec4<float>;
+	using Pos4D = Vec4<double>;
+
+
+	using Color3B = Vec3<uint8_t>;
+	using Color3F = Vec3<float>;
+
+	using Color4B = Vec4<uint8_t>;
+	using Color4F = Vec4<float>;
 
 /*****************************************************/
 //				Functions
